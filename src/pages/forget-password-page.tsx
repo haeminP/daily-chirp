47c42be81ca397a24bb73b82fb9f291b876fc66e
Haemin Park <park86@uwindsor.ca>
2025-12-22 00:57:18 -0500

    implemented reset password

--- src/pages/forget-password-page.tsx
+++ src/pages/forget-password-page.tsx
@@ -1,3 +1,59 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useRequestPasswordResetEmail } from "@/hooks/mutations/use-request-password-reset-email";
+import { generateErrorMessage } from "@/lib/error";
+import { useState } from "react";
+import { toast } from "sonner";
+
 export default function ForgetPasswordPage() {
-  return <div>ForgetPasswordPage</div>;
+  const [email, setEmail] = useState("");
+
+  const {
+    mutate: requestPasswordResetEmail,
+    isPending: isRequestPasswordResetEmailPending,
+  } = useRequestPasswordResetEmail({
+    onSuccess: () => {
+      toast.info("Password reset email has been sent.", {
+        position: "top-center",
+      });
+      setEmail("");
+    },
+    onError: (error) => {
+      const message = generateErrorMessage(error);
+      toast.error(message, {
+        position: "top-center",
+      });
+      setEmail("");
+    },
+  });
+
+  const handleSendEmailClick = () => {
+    if (email.trim() === "") return;
+    requestPasswordResetEmail(email);
+  };
+
+  return (
+    <div className="flex flex-col gap-8">
+      <div className="flex flex-col gap-1">
+        <div className="text-xl font-bold">Forgot your password?</div>
+        <div className="text-muted-foreground">
+          Get an email to reset your password
+        </div>
+      </div>
+      <Input
+        value={email}
+        disabled={isRequestPasswordResetEmailPending}
+        onChange={(e) => setEmail(e.target.value)}
+        className="py-6"
+        placeholder="example@abc.com"
+      />
+      <Button
+        onClick={handleSendEmailClick}
+        disabled={isRequestPasswordResetEmailPending}
+        className="w-full"
+      >
+        Get an email for password reset
+      </Button>
+    </div>
+  );
 }
--- src/pages/reset-password-page.tsx
+++ src/pages/reset-password-page.tsx
@@ -1,3 +1,57 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useUpdatePassword } from "@/hooks/mutations/use-update-password";
+import { generateErrorMessage } from "@/lib/error";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { toast } from "sonner";
+
 export default function ResetPasswordPage() {
-  return <div>ResetPasswordPage</div>;
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const { mutate: updatePassword, isPending: isUpdatePasswordPending } =
+    useUpdatePassword({
+      onSuccess: () => {
+        toast.info("Succesfully updated the password.", {
+          position: "top-center",
+        });
+        navigate("/");
+      },
+      onError: (error) => {
+        const message = generateErrorMessage(error);
+        toast.error(message, {
+          position: "top-center",
+        });
+        setPassword("");
+      },
+    });
+  const handleUpdatePasswordClick = () => {
+    if (password.trim() === "") return;
+    updatePassword(password);
+  };
+
+  return (
+    <div className="flex flex-col gap-8">
+      <div className="flex flex-col gap-1">
+        <div className="text-xl font-bold">Reset Password</div>
+        <div className="text-muted-foreground">Enter a new password</div>
+      </div>
+      <Input
+        value={password}
+        disabled={isUpdatePasswordPending}
+        onChange={(e) => setPassword(e.target.value)}
+        type="password"
+        className="py-6"
+        placeholder="password"
+      />
+      <Button
+        onClick={handleUpdatePasswordClick}
+        disabled={isUpdatePasswordPending}
+        className="w-full"
+      >
+        Reset password
+      </Button>
+    </div>
+  );
 }
