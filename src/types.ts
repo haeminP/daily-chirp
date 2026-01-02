a22dad6153efa68ce9c79ab221ae184d1658efcd
Haemin Park <park86@uwindsor.ca>
2026-01-02 14:08:48 -0500

    added profile table to the database

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,8 @@ import { type Database } from "@/database.types";
 
 export type PostEntity = Database["public"]["Tables"]["post"]["Row"];
 
+export type ProfileEntity = Database["public"]["Tables"]["profile"]["Row"];
+
 export type useMutationCallback = {
   onSuccess?: () => void;
   onError?: (error: Error) => void;
