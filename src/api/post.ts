ef5de7cf11584737f9f645f6550a65fba9cde29b
Haemin Park <park86@uwindsor.ca>
2026-01-06 16:27:08 -0500

    saving images to storage and send the upload request with CreatePost

--- src/api/post.ts
+++ src/api/post.ts
@@ -1,9 +1,79 @@
 import supabase from "@/lib/supabase";
+import { uploadImage } from "./image";
+import type { PostEntity } from "@/types";
 
 export async function createPost(content: string) {
-  const { data, error } = await supabase.from("post").insert({
-    content,
-  });
+  const { data, error } = await supabase
+    .from("post")
+    .insert({
+      content,
+    })
+    .select()
+    .single();
+
+  if (error) throw error;
+  return data;
+}
+
+export async function createPostWithImages({
+  content,
+  images,
+  userId,
+}: {
+  content: string;
+  images: File[];
+  userId: string;
+}) {
+  // 1. create a new post
+  const post = await createPost(content);
+  if (images.length === 0) return post;
+
+  try {
+    // 2. upload images to storage with posting Id
+    const imageUrls = await Promise.all(
+      images.map((image) => {
+        const fileExtension = image.name.split(".").pop() || "webp"; // get the file fileExtension
+        const fileName = `${Date.now()}-${crypto.randomUUID()}.${fileExtension}`;
+        const filePath = `${userId}/${post.id}/${fileName}`;
+        return uploadImage({
+          file: image,
+          filePath,
+        });
+      })
+    );
+
+    // 3. update post table
+    const updatedPost = await updatePost({
+      id: post.id,
+      image_urls: imageUrls,
+    });
+
+    return updatedPost;
+  } catch (error) {
+    await deletePost(post.id);
+    throw error;
+  }
+}
+
+export async function updatePost(post: Partial<PostEntity> & { id: number }) {
+  const { data, error } = await supabase
+    .from("post")
+    .update(post)
+    .eq("id", post.id)
+    .select()
+    .single();
+
+  if (error) throw error;
+  return data;
+}
+
+export async function deletePost(id: number) {
+  const { data, error } = await supabase
+    .from("post")
+    .delete()
+    .eq("id", id)
+    .select()
+    .single();
 
   if (error) throw error;
   return data;
