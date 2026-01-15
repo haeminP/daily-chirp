1e6ed733e1b71042855b55b855ea4f708a86f398
Haemin Park <park86@uwindsor.ca>
2026-01-15 16:19:10 -0500

    feat: add comment type definitions and update delete post functionality to navigate after deletion

--- src/components/post/delete-post-button.tsx
+++ src/components/post/delete-post-button.tsx
@@ -2,10 +2,18 @@ import { useOpenAlertModal } from "@/store/alert-modal";
 import { Button } from "../ui/button";
 import { useDeletePost } from "@/hooks/mutations/post/use-delete-post";
 import { toast } from "sonner";
+import { useNavigate } from "react-router";
 
 export default function DeletePostButton({ id }: { id: number }) {
   const openAlertModal = useOpenAlertModal();
+  const navigate = useNavigate();
   const { mutate: deletePost, isPending: isDeletePostPending } = useDeletePost({
+    onSuccess: () => {
+      const pathName = window.location.pathname;
+      if (pathName.startsWith(`/post/${id}`)) {
+        navigate("/", { replace: true });
+      }
+    },
     onError: (error) => {
       toast.error("Cannot delete the post", { position: "top-center" });
     },
--- src/types.ts
+++ src/types.ts
@@ -3,7 +3,7 @@ import { type Database } from "@/database.types";
 export type PostEntity = Database["public"]["Tables"]["post"]["Row"];
 export type ProfileEntity = Database["public"]["Tables"]["profile"]["Row"];
 export type Post = PostEntity & { author: ProfileEntity; isLiked: boolean };
-
+export type CommentEntity = Database["public"]["Tables"]["comment"]["Row"];
 export type useMutationCallback = {
   onSuccess?: () => void;
   onError?: (error: Error) => void;
